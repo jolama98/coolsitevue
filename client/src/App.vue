1faa472d4ace34eb1da715457766f1cc74840782
jo <josehehall98@gmail.com>
2024-09-25 23:02:23 -0600

    push

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
-import { AppState } from './AppState.js';
 
 </script>
 
@@ -38,10 +37,6 @@ import { AppState } from './AppState.js';
 
 <style lang="scss">
 @import '@/assets/scss/main.scss';
-
-:root {
-  --main-height: calc(100vh - 32px - 64px);
-}
 
 main {
   background-color: #061130;
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -3,17 +3,28 @@
 </script>
 
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6">
+
+  <div class="row">
+    <div class="col-md-6">
+      <div class="">
         <p>Services</p>
-        <p>Building web apps</p>
+        <p class="fs-2 text-light d-flex justify-content-start">Building web apps that scale to your world wide
+          audience</p>
+        <p class="text-light">At CS2 we identify the digital divide with additional
+          clickthroughs from DevOps
+          Nanotechnologt immersion along the information highway to close the loop on focusing solely on the bottom
+          line. </p>
       </div>
-      <div class="col-md-6">
-        <img class="img-fluid" src="https://cwcurriculum.blob.core.windows.net/fullstack/assets/img/world.png" alt="">
-      </div>
+    </div>
+    <div class="col-md-6 align-self-center">
+      <img class="img-fluid mt-5 pt-5" src="https://cwcurriculum.blob.core.windows.net/fullstack/assets/img/world.png"
+        alt="">
     </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+p {
+  color: #FAFD61;
+}
+</style>
