6260cb0ceca841a001e11155b3a36052c7727fbf
jo <josehehall98@gmail.com>
2024-09-26 17:40:50 -0600

    push

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,27 +11,24 @@ import Navbar from './components/Navbar.vue';
     <router-view />
   </main>
   <footer>
-    <div class="row">
-      <div class="col-12">
-        <div class="d-flex m-3 pb-3 justify-content-around ">
-          <a href=" https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs">HTML & CSS</a>
-          <a href=" https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs">Semantic Markup</a>
-          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs">Responsive Design</a>
-          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs">Accessibility
-            Standards</a>
+    <div class="col-12">
+      <div class="d-flex justify-content-around p-4">
+        <a href=" https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs">HTML & CSS</a>
+        <a href=" https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs">Semantic Markup</a>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs">Responsive Design</a>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs">Accessibility
+          Standards</a>
+      </div>
+    </div>
+    <div class="col-12">
+      <div class="lower-footer d-flex p-4 justify-content-center">
+        <div class="d-block">
+          <p class="fs-2 fw-semibold d-flex justify-content-center">From Ideas to Reality</p>
+          <p>If you can dream it then you can build it with technology solutions.</p>
         </div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-12">
-        <div class="lower-footer d-flex justify-content-center">
-          <div class="d-block">
-            <p class="fs-2 fw-semibold">From Ideas to Reality</p>
-            <p>If you can dream it then you can build it with technology solutions.</p>
-          </div>
-        </div>
-      </div>
-    </div>
+
   </footer>
 </template>
 
@@ -44,12 +41,9 @@ main {
 
 .lower-footer {
   background-color: #dedede;
-  height: 25vh;
 }
 
 footer {
-  height: 20vh;
   background-color: #F4F6F8;
-
 }
 </style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -3,22 +3,28 @@
 </script>
 
 <template>
+  <div class="container">
 
-  <div class="row">
-    <div class="col-md-6">
-      <div class="">
-        <p>Services</p>
-        <p class="fs-2 text-light d-flex justify-content-start">Building web apps that scale to your world wide
-          audience</p>
-        <p class="text-light">At CS2 we identify the digital divide with additional
-          clickthroughs from DevOps
-          Nanotechnologt immersion along the information highway to close the loop on focusing solely on the bottom
-          line. </p>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="d-flex flex-column align-items-center ">
+          <p class="fs-4">Services</p>
+          <p class="fs-2 text-light">Building web apps that scale to your world wide
+            audience. <br>
+            <span class="fs-5">
+              At CS2 we identify the digital divide with additional
+              clickthroughs from DevOps
+              Nanotechnologt immersion along the information highway to close the loop on focusing solely on the bottom
+              line.
+            </span>
+          </p>
+        </div>
       </div>
-    </div>
-    <div class="col-md-6 align-self-center">
-      <img class="img-fluid mt-5 pt-5" src="https://cwcurriculum.blob.core.windows.net/fullstack/assets/img/world.png"
-        alt="">
+      <div class="col-md-6">
+        <div class="d-flex flex-row align-items-start">
+          <img class="" src="https://cwcurriculum.blob.core.windows.net/fullstack/assets/img/world.png" alt="">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -26,5 +32,9 @@
 <style scoped lang="scss">
 p {
   color: #FAFD61;
+}
+img{
+  object-position: 50% 50%;
+
 }
 </style>
